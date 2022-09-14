ba186efdf8288c267c58a488955b1fe9679be0b4
edr3x <dhunganaannanta@gmail.com>
2022-09-15 01:17:34 +0545

    small change in line

--- .config/nvim/after/plugin/line.lua
+++ .config/nvim/after/plugin/line.lua
@@ -3,7 +3,7 @@ require('lualine').setup {
         icons_enabled = true,
         theme = 'tokyonight',
         component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         disabled_filetypes = {},
         always_divide_middle = true,
         globalstatus = false,
