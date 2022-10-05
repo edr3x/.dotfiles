fb42edbfd9c565b43b6f23a44cc7ff8d277f7f8b
edr3x <dhunganaannanta@gmail.com>
2022-10-05 19:31:54 +0545

    fix: keybinds.nvim

--- nvim/.config/nvim/lua/r3x/mappings.lua
+++ nvim/.config/nvim/lua/r3x/mappings.lua
@@ -5,18 +5,19 @@ vim.g.mapleader = ','
 
 map('n', 'ff', ':Telescope find_files<cr>', options)
 map('n', '<leader>ntt', ':NvimTreeToggle<CR>', options)
+map('n', '<leader>b', ':bprev<CR>', options)
+map('n', '<leader>sd', ':Telescope flutter commands<CR>', options)
 
 map('x', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', options)
 map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', options)
-map('n', '<leader>sd', ':Telescope flutter commands<cr>', options)
-map("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", options)
-map("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", options)
-map("n", "<leader>gr", "<cmd>lua vim.lsp.buf.reference()<CR>", options)
-map('n', '<leader>t', ':lua vim.lsp.buf.hover()<cr>', options)
-map("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", options)
-map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", options)
-map("n", "<C-n>", "<cmd>lua vim.diagnostic.goto_prev()<CR>", options)
-map("n", "<C-p>", "<cmd>lua vim.diagnostic.goto_next()<CR>", options)
+map('n', '<leader>df', '<cmd>lua vim.lsp.buf.definition()<CR>', options)
+map('n', '<leader>de', '<cmd>lua vim.lsp.buf.declaration()<CR>', options)
+map('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', options)
+map('n', '<leader>rf', '<cmd>lua vim.lsp.buf.reference()<CR>', options)
+map('n', '<leader>t', '<cmd>lua vim.lsp.buf.hover()<CR>', options)
+map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', options)
+map('n', '<C-n>', '<cmd>lua vim.diagnostic.goto_prev()<CR>', options)
+map('n', '<C-p>', '<cmd>lua vim.diagnostic.goto_next()<CR>', options)
 
 --debugger
 map("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", options)
