1886e82fc721a5ba9b72de5e62d8b4e48cf8bd0b
edr3x <dhunganaannanta@gmail.com>
2022-10-20 18:30:13 +0545

    feat: show active lsp in lualine

--- nvim/.config/nvim/after/plugin/line.lua
+++ nvim/.config/nvim/after/plugin/line.lua
@@ -1,7 +1,27 @@
+local lspStatus = {
+    function()
+        local msg = "No Active Lsp"
+        local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+        local clients = vim.lsp.get_active_clients()
+        if next(clients) == nil then
+            return msg
+        end
+        for _, client in ipairs(clients) do
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                return client.name
+            end
+        end
+        return msg
+    end,
+    icon = "",
+    color = { fg = "#d3d3d3" }
+}
+
 require('lualine').setup {
     options = {
         icons_enabled = true,
-        theme = 'tokyonight',
+        theme = 'auto',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {},
@@ -12,7 +32,7 @@ require('lualine').setup {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = { lspStatus, 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
     },
