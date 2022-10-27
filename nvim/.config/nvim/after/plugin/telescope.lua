683c300bc5798cbff3c0081c434975a6188b7604
edr3x <dhunganaannanta@gmail.com>
2022-10-27 20:59:10 +0545

    feat(nvim-telescope): esc binds and minor refactor

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,12 +1,19 @@
-require('telescope').setup {
+local scope = require('telescope')
+local esc = { ["<esc>"] = require('telescope.actions').close }
+
+scope.setup {
     defaults = {
         file_ignore_patterns = { "%.git$", "%.git[/\\]", "node_modules", "target", "build", "ios", "android", "linux",
             "macos", "web", "windows", "%.lock$" },
         color_devicons = true,
+        mappings = {
+            i = esc,
+            n = esc,
+        },
     }
 }
 
-require('telescope').load_extension('flutter')
-require('telescope').load_extension('harpoon')
-require('telescope').load_extension('dap')
-require('telescope').load_extension('emoji')
+scope.load_extension('flutter')
+scope.load_extension('harpoon')
+scope.load_extension('dap')
+scope.load_extension('emoji')
