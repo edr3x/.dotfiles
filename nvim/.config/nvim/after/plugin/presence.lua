3bbf79e44c0a7f083edf075f8f1683ff47e1690b
edr3x <dhunganaannanta@gmail.com>
2022-11-01 14:13:40 +0545

    feat(custom conf): custom presence message

---
+++ nvim/.config/nvim/after/plugin/presence.lua
@@ -0,0 +1,10 @@
+require("presence"):setup({
+    auto_update        = true,
+    show_time          = true,
+    neovim_image_text  = "HELP!, couldn't exit.",
+    main_image         = "file",
+    editing_text       = "Editing %s",
+    file_explorer_text = "Browsing %s",
+    reading_text       = "Reading %s",
+    workspace_text     = "Working on %s",
+})
