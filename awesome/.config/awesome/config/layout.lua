1ce2666e57392913b67cb018caf85de6f8938a30
edr3x <dhunganaannanta@gmail.com>
2022-11-14 12:10:23 +0545

    feat(awesome): some configs

---
+++ awesome/.config/awesome/config/layout.lua
@@ -0,0 +1,19 @@
+local awful = require("awful")
+
+tag.connect_signal("request::default_layouts", function()
+    awful.layout.append_default_layouts({
+        awful.layout.suit.fair,
+        awful.layout.suit.tile,
+        awful.layout.suit.spiral,
+        awful.layout.suit.floating,
+        --awful.layout.suit.tile.left,
+        --awful.layout.suit.tile.bottom,
+        --awful.layout.suit.tile.top,
+        --awful.layout.suit.spiral.dwindle,
+        --awful.layout.suit.max,
+        --awful.layout.suit.fair.horizontal,
+        --awful.layout.suit.max.fullscreen,
+        --awful.layout.suit.magnifier,
+        --awful.layout.suit.corner.nw,
+    })
+end)
