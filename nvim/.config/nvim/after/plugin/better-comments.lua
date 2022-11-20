5ba6a03a3786707aad09d190cee8785b51495d73
edr3x <dhunganaannanta@gmail.com>
2022-11-20 23:28:46 +0545

    refactor(comments): fixed little things

---
+++ nvim/.config/nvim/after/plugin/better-comments.lua
@@ -0,0 +1,15 @@
+local status, comments = pcall(require, "todo-comments")
+if not status then
+    return
+end
+
+comments.setup {
+    keywords = {
+        FIX  = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "myth" } },
+        TODO = { icon = " ", color = "#FF8C00" },
+        HACK = { icon = " ", color = "#3498DB" },
+        WARN = { icon = " ", color = "#FF2D00", alt = { "WARNING", "XXX", "!" } },
+        NOTE = { icon = " ", color = "#98C379", alt = { "INFO", "*" } },
+    },
+    highlight = { pattern = [[(KEYWORDS)]] },
+}
