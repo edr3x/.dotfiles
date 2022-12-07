1f7dd159f99283813aea3df55d8a950900d35587
edr3x <dhunganaannanta@gmail.com>
2022-12-07 13:44:36 +0545

    refactor: shorten

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -1,120 +1,14 @@
-local status, rtools = pcall(require, "rust-tools")
+local status, rust_tools = pcall(require, "rust-tools")
 if not status then
     return
 end
 
-local stat, rxt = pcall(require, "rust-tools/executors")
-if not stat then
-    return
-end
-
 local opts = {
-    tools = {
-        autoSetHints = true,
-        executor = rxt.termopen,
-        on_initialized = nil,
-        inlay_hints = {
-            only_current_line = false,
-            only_current_line_autocmd = "CursorHold",
-            show_parameter_hints = true,
-            show_variable_name = false,
-            parameter_hints_prefix = "<- ",
-            other_hints_prefix = "=> ",
-            max_len_align = false,
-            max_len_align_padding = 1,
-            right_align = false,
-            right_align_padding = 7,
-            highlight = "Comment",
-        },
-        hover_actions = {
-            border = {
-                { "╭", "FloatBorder" },
-                { "─", "FloatBorder" },
-                { "╮", "FloatBorder" },
-                { "│", "FloatBorder" },
-                { "╯", "FloatBorder" },
-                { "─", "FloatBorder" },
-                { "╰", "FloatBorder" },
-                { "│", "FloatBorder" },
-            },
-
-            auto_focus = false,
-        },
-        crate_graph = {
-            backend = "x11",
-            output = nil,
-            full = true,
-            enabled_graphviz_backends = {
-                "bmp",
-                "cgimage",
-                "canon",
-                "dot",
-                "gv",
-                "xdot",
-                "xdot1.2",
-                "xdot1.4",
-                "eps",
-                "exr",
-                "fig",
-                "gd",
-                "gd2",
-                "gif",
-                "gtk",
-                "ico",
-                "cmap",
-                "ismap",
-                "imap",
-                "cmapx",
-                "imap_np",
-                "cmapx_np",
-                "jpg",
-                "jpeg",
-                "jpe",
-                "jp2",
-                "json",
-                "json0",
-                "dot_json",
-                "xdot_json",
-                "pdf",
-                "pic",
-                "pct",
-                "pict",
-                "plain",
-                "plain-ext",
-                "png",
-                "pov",
-                "ps",
-                "ps2",
-                "psd",
-                "sgi",
-                "svg",
-                "svgz",
-                "tga",
-                "tiff",
-                "tif",
-                "tk",
-                "vml",
-                "vmlz",
-                "wbmp",
-                "webp",
-                "xlib",
-                "x11",
-            },
-        },
-    },
-
     server = {
-        standalone = true,
-    }, -- rust-analyer options
-
-    dap = {
-        adapter = {
-            type = "executable",
-            command = "lldb-vscode",
-            name = "rt_lldb",
-        },
+        on_attach = require("r3x.lsp-handlers").on_attach,
+        standalone = false,
     },
 }
 
-rtools.setup(opts)
-rtools.inlay_hints.enable()
+rust_tools.setup(opts)
+rust_tools.inlay_hints.enable()
