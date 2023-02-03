a3cd5c3dde05caf7f457647a95b59e56616b141d
edr3x <dhunganaannanta@gmail.com>
2023-02-03 15:19:59 +0545

    refactor: reduced some lines of unnecessary code

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,18 +1,3 @@
-local mason_status, mason = pcall(require, "mason")
-if not mason_status then
-    return
-end
-
-local mason_lsp_status, masonlsp = pcall(require, "mason-lspconfig")
-if not mason_lsp_status then
-    return
-end
-
-local lsp_status, lspconf = pcall(require, "lspconfig")
-if not lsp_status then
-    return
-end
-
 local servers = {
     'clangd',
     'sumneko_lua',
@@ -30,37 +15,28 @@ local servers = {
     'jdtls',
 }
 
-mason.setup {
-    ui = {
-        border = "none",
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        },
-    },
-    log_level = vim.log.levels.INFO,
-    max_concurrent_installers = 4,
-}
-
-masonlsp.setup {
+local opts = {
     ensure_installed = servers,
     automatic_installation = true,
 }
 
+local lsp_settings = {
+    Lua = {
+        diagnostics = {
+            globals = { 'vim', 'awesome', 'client' }
+        }
+    }
+}
+
+require("mason").setup()
+require("mason-lspconfig").setup(opts)
 for _, lsp in pairs(servers) do
-    lspconf[lsp].setup {
+    require("lspconfig")[lsp].setup {
         on_attach = require("r3x.lsp-handlers").on_attach,
         capabilities = require("r3x.lsp-handlers").capabilities,
         flags = {
             debounce_text_changes = 150,
         },
-        settings = {
-            Lua = {
-                diagnostics = {
-                    globals = { 'vim', 'awesome', 'client' }
-                }
-            }
-        }
+        settings = lsp_settings,
     }
 end
