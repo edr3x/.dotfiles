78c208208d30ea9d23c01e2d2de2a6d1ac12e745
edr3x <dhunganaannanta@gmail.com>
2023-02-04 15:22:06 +0545

    refactor: lazy load peek on md type

--- nvim/.config/nvim/lua/r3x/plugins/peek.lua
+++ nvim/.config/nvim/lua/r3x/plugins/peek.lua
@@ -1,5 +1,6 @@
 return {
     'toppair/peek.nvim',
+    ft = 'markdown',
     build = 'deno task --quiet build:fast',
     commit = "18284b64f6c5103ccf18d6b53f288d2b2de2dc4a",
     config = function()
