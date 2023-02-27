1fa0bbcb4d6088def9f5ce98f8bf434ab19f11d3
edr3x <dhunganaannanta@gmail.com>
2023-02-27 16:07:28 +0545

    feat: indent ignore

--- nvim/.config/nvim/lua/r3x/plugins/indent.lua
+++ nvim/.config/nvim/lua/r3x/plugins/indent.lua
@@ -6,5 +6,16 @@ return {
         space_char_blankline = " ",
         show_current_context = false,
         show_current_context_start = false,
+        filetype_exclude = {
+            "vimwiki",
+            "man",
+            "diagnosticpopup",
+            "lspinfo",
+            "lazy",
+            "checkhealth",
+            "TelescopePrompt",
+            "TelescopeResults",
+            "",
+        },
     }
 }
