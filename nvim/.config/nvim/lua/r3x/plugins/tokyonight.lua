9f77375984ab784519b167ea55487c4ae8ca3d23
edr3x <dhunganaannanta@gmail.com>
2023-03-15 21:31:16 +0545

    feat: sidebar highlight

--- nvim/.config/nvim/lua/r3x/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/r3x/plugins/tokyonight.lua
@@ -6,10 +6,19 @@ return {
         transparent = true,
         terminal_colors = true,
         styles = {
+            -- Value is any valid attr-list value for `:help nvim_set_hl`
             comments = { italic = true },
-            keywords = { italic = true },
+            keywords = { italic = false },
+            functions = {},
+            variables = {},
             sidebars = "transparent",
             floats = "transparent",
         },
+        on_highlights = function(hl, c)
+            hl.CursorLineNr = { fg = c.orange, bold = true }
+            hl.LineNr = { fg = c.orange, bold = true }
+            hl.LineNrAbove = { fg = c.fg_gutter }
+            hl.LineNrBelow = { fg = c.fg_gutter }
+        end,
     }
 }
