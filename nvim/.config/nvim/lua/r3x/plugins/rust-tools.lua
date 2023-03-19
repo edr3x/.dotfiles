13845e6fcff0a6e1af8ddbf5a499a0d9a91533e7
edr3x <dhunganaannanta@gmail.com>
2023-03-20 00:10:23 +0545

    refactor(rust): lsp and remove defaults

--- nvim/.config/nvim/lua/r3x/plugins/rust-tools.lua
+++ nvim/.config/nvim/lua/r3x/plugins/rust-tools.lua
@@ -2,27 +2,11 @@ return {
     "simrat39/rust-tools.nvim",
     ft = "rust",
     opts = {
-        tools = {
-            autoSetHints = true,
-            inlay_hints = {
-                auto = true,
-                only_current_line = false,
-                only_current_line_autocmd = "CursorHold",
-                show_parameter_hints = true,
-                show_variable_name = false,
-                parameter_hints_prefix = "<- ",
-                other_hints_prefix = "=> ",
-                max_len_align = false,
-                max_len_align_padding = 1,
-                right_align = false,
-                right_align_padding = 7,
-                highlight = "Comment",
-            },
-        },
         server = {
-            standalone = true,
+            standalone = false,
             on_attach = require("r3x.handlers").on_attach,
             capabilities = require("r3x.handlers").capabilities,
+            cmd = { "rustup", "run", "stable", "rust-analyzer" }, --TODO: `rustup component add rust-analyzer` to install LSP
         },
     },
 }
