35cc2692d2c284434549e93bc091bb213118a9cd
Rony Lee <rongyil33@gmail.com>
2023-03-27 19:24:44 +0800

    Add modkey left/right click for moving/resizing floating window

--- awesome/config/keybinds.lua
+++ awesome/config/keybinds.lua
@@ -365,3 +365,17 @@ awful.mouse.append_global_mousebindings({
     --awful.button({}, 4, awful.tag.viewprev),
     --awful.button({}, 5, awful.tag.viewnext),
 })
+
+client.connect_signal("request::default_mousebindings", function()
+    awful.mouse.append_client_mousebindings({
+        awful.button({ }, 1, function (c)
+            c:activate { context = "mouse_click" }
+        end),
+        awful.button({ modkey }, 1, function (c)
+            c:activate { context = "mouse_click", action = "mouse_move"  }
+        end),
+        awful.button({ modkey }, 3, function (c)
+            c:activate { context = "mouse_click", action = "mouse_resize"}
+        end),
+    })
+end)
