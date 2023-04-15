f72c9d483833c813723afd7cf6db5609a202fa18
edr3x <dhunganaannanta@gmail.com>
2023-04-15 18:51:17 +0545

    feat(vim-tmux): tmux-vim-navigator for seamless jumps of splits and panes

--- nvim/.config/nvim/lua/r3x/keymaps.lua
+++ nvim/.config/nvim/lua/r3x/keymaps.lua
@@ -45,11 +45,11 @@ map("n", "<C-Down>", ":resize +2<CR>", opts)
 map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
--- split navigation
-map("n", "<C-h>", "<C-w>h", opts)
-map("n", "<C-j>", "<C-w>j", opts)
-map("n", "<C-k>", "<C-w>k", opts)
-map("n", "<C-l>", "<C-w>l", opts)
+-- split navigation(commented couz of tmux-vim-navigator plugin)
+-- map("n", "<C-h>", "<C-w>h", opts)
+-- map("n", "<C-j>", "<C-w>j", opts)
+-- map("n", "<C-k>", "<C-w>k", opts)
+-- map("n", "<C-l>", "<C-w>l", opts)
 
 -- term mode
 map("t", "<esc>", "<C-\\><C-n>", opts) -- go back to normal mode with <esc> on terminal mode
