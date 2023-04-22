75e2ac50ab3db60e857f85c863fd9c45fbef7c41
edr3x <dhunganaannanta@gmail.com>
2023-04-22 15:48:35 +0545

    refactor: remove pinned commit form peek

--- nvim/.config/nvim/lua/r3x/plugins/peek.lua
+++ nvim/.config/nvim/lua/r3x/plugins/peek.lua
@@ -2,7 +2,6 @@ return {
     "toppair/peek.nvim",
     ft = "markdown",
     build = "deno task --quiet build:fast",
-    commit = "18284b64f6c5103ccf18d6b53f288d2b2de2dc4a",
     keys = {
         {
             "<leader>md",
