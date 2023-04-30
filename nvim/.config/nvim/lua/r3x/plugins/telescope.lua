a0b76311754838ebf6c4956435c999ba7ba2b6b6
edr3x <dhunganaannanta@gmail.com>
2023-04-30 17:32:23 +0545

    feat: use delta for telescope git status preview

--- nvim/.config/nvim/lua/r3x/plugins/telescope.lua
+++ nvim/.config/nvim/lua/r3x/plugins/telescope.lua
@@ -14,57 +14,94 @@ return {
         { "<leader>fs", "<cmd>Telescope symbols<CR>", desc = "List symbols" },
         { "<leader>fc", "<cmd>Telescope flutter commands<CR>", desc = "Flutter Commands" },
         { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Git status" },
+        { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Git status" },
         { "<leader>gt", "<cmd>Telescope git_branches<CR>", desc = "Git branches" },
     },
-    opts = {
-        defaults = {
-            file_ignore_patterns = {
-                ".git/",
-                "node_modules",
-                "target",
-                "build",
-                "ios",
-                "android",
-                "linux",
-                "macos",
-                "web",
-                "windows",
-                "%.lock$",
+    opts = function()
+        local delta = require("telescope.previewers").new_termopen_previewer({
+            get_command = function(entry)
+                if entry.status == "??" or "A " then
+                    return { "git", "-c", "core.pager=delta", "-c", "delta.side-by-side=false", "diff", entry.value }
+                end
+
+                return { "git", "-c", "core.pager=delta", "-c", "delta.side-by-side=false", "diff", entry.value .. "^!" }
+            end,
+        })
+
+        return {
+            defaults = {
+                file_ignore_patterns = {
+                    ".git/",
+                    "node_modules",
+                    "target",
+                    "build",
+                    "ios",
+                    "android",
+                    "linux",
+                    "macos",
+                    "web",
+                    "windows",
+                    "%.lock$",
+                },
+                color_devicons = true,
+                mappings = {
+                    n = {
+                        ["dd"] = "delete_buffer",
+                    },
+                },
+                prompt_prefix = "   ",
+                initial_mode = "insert",
+                selection_strategy = "reset",
+                sorting_strategy = "ascending",
+                layout_strategy = "horizontal",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        preview_width = 0.60,
+                    },
+                    vertical = {
+                        mirror = false,
+                    },
+                    width = 0.90,
+                    height = 0.85,
+                    preview_cutoff = 0,
+                },
+                path_display = { "smart", shorten = { len = 3 } },
+                dynamic_preview_title = true,
+                wrap_results = true,
             },
-            color_devicons = true,
-            mappings = {
-                n = {
-                    ["dd"] = "delete_buffer",
+            pickers = {
+                git_status = {
+                    prompt_prefix = "󰊢 ",
+                    show_untracked = true,
+                    initial_mode = "normal",
+                    previewer = delta,
+                },
+                git_commits = {
+                    prompt_prefix = "󰊢 ",
+                    initial_mode = "normal",
+                    git_command = { "git", "log", "--all", "--pretty=%h %s (%cr)", "--", "." },
+                    results_title = "git log (current buffer)",
+                    previewer = delta,
+                },
+                git_bcommits = {
+                    prompt_prefix = "󰊢 ",
+                    initial_mode = "normal",
+                    git_command = { "git", "log", "--all", "--pretty=%h %s (%cr)" },
+                    results_title = "git log",
+                    previewer = delta,
+                },
+                git_branches = {
+                    prompt_prefix = "󰊢 ",
+                    initial_mode = "normal",
+                },
+                find_files = { hidden = true },
+                live_grep = {
+                    additional_args = function()
+                        return { "--hidden" }
+                    end,
                 },
             },
-            prompt_prefix = "   ",
-            initial_mode = "insert",
-            selection_strategy = "reset",
-            sorting_strategy = "ascending",
-            layout_strategy = "horizontal",
-            layout_config = {
-                horizontal = {
-                    prompt_position = "top",
-                    preview_width = 0.60,
-                },
-                vertical = {
-                    mirror = false,
-                },
-                width = 0.90,
-                height = 0.85,
-                preview_cutoff = 0,
-            },
-            path_display = { "smart", shorten = { len = 3 } },
-            dynamic_preview_title = true,
-            wrap_results = true,
-        },
-        pickers = {
-            find_files = { hidden = true },
-            live_grep = {
-                additional_args = function()
-                    return { "--hidden" }
-                end,
-            },
-        },
-    },
+        }
+    end,
 }
--- nvim/.config/nvim/lua/r3x/plugins/trouble.lua
+++ nvim/.config/nvim/lua/r3x/plugins/trouble.lua
@@ -7,6 +7,7 @@ return {
             { "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
             { "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Current Buffer Diagnostics" },
             { "<leader>dr", "<cmd>TroubleToggle lsp_references<cr>", desc = "References" },
+            { "<leader>tq", "<cmd>TroubleToggle quickfix<cr>", desc = "Open quickfix List" },
         },
         opts = {},
     },
