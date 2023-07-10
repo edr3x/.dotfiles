f788ff39cda28fbd7b78d7633bf31b5c14b3c6fd
edr3x <dhunganaannanta@gmail.com>
2023-07-10 12:27:21 +0545

    `chore:` gopls settings change

--- nvim/.config/nvim/lua/r3x/plugins/lsp.lua
+++ nvim/.config/nvim/lua/r3x/plugins/lsp.lua
@@ -101,20 +101,15 @@ return {
                     gopls = {
                         completeUnimported = true,
                         usePlaceholders = true,
+                        gofumpt = true,
                         staticcheck = true,
                         analyses = {
                             unusedparams = true,
                         },
-                        hints = {
-                            assignVariableTypes = true,
-                            compositeLiteralFields = true,
-                            compositeLiteralTypes = true,
-                            constantValues = true,
-                            functionTypeParameters = true,
-                            parameterNames = true,
-                            rangeVariableTypes = true,
-                        },
                     },
+                },
+                flags = {
+                    debounce_text_changes = 120,
                 },
             })
 
