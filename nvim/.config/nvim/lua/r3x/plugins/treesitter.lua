2cd48d90274ee014fff1b4e0e4a5a26bf2fe155e
edr3x <dhunganaannanta@gmail.com>
2023-08-01 10:08:12 +0545

    chore: some lint error fixes

--- nvim/.config/nvim/lua/r3x/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/r3x/plugins/treesitter.lua
@@ -1,88 +1,91 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = "BufReadPre",
-        build = function()
-            require("nvim-treesitter.install").update({ with_sync = true })
-        end or ":TSUpdate",
-        dependencies = {
-            "windwp/nvim-ts-autotag",
-            "nvim-treesitter/playground",
-            "JoosepAlviste/nvim-ts-context-commentstring",
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufReadPre",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })
+    end or ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+        "nvim-treesitter/playground",
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        {
+            "nvim-treesitter/nvim-treesitter-context",
+            opts = {},
         },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "cpp",
-                    "css",
-                    "dart",
-                    "dockerfile",
-                    "fish",
-                    "go",
-                    "graphql",
-                    "html",
-                    "java",
-                    "javascript",
-                    "json",
-                    "llvm",
-                    "lua",
-                    "make",
-                    "markdown",
-                    "prisma",
-                    "proto",
-                    "python",
-                    "query",
-                    "rust",
-                    "scss",
-                    "svelte",
-                    "sql",
-                    "toml",
-                    "tsx",
-                    "typescript",
-                    "vimdoc",
-                    "yaml",
-                },
-                autotag = { enable = true },
-                auto_install = false,
-                highlight = {
-                    enable = true,
-                    extended_mode = true,
-                    disable = function(_, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
-                    end,
-                    additional_vim_regex_highlighting = false,
-                },
-                context_commentstring = {
-                    enable = true,
-                    enable_autocmd = false,
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<c-space>",
-                        node_incremental = "<c-space>",
-                        scope_incremental = "<c-s>",
-                        node_decremental = "<c-backspace>",
-                    },
-                },
-                playground = { enable = true },
-                query_linter = {
-                    enable = true,
-                    use_virtual_text = true,
-                    lint_events = { "BufWrite", "CursorHold" },
-                },
-            })
-        end,
     },
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        event = "BufReadPre",
-        opts = {},
-    },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "css",
+                "dart",
+                "dockerfile",
+                "fish",
+                "go",
+                "graphql",
+                "html",
+                "java",
+                "javascript",
+                "json",
+                "llvm",
+                "lua",
+                "make",
+                "markdown",
+                "prisma",
+                "proto",
+                "python",
+                "query",
+                "rust",
+                "scss",
+                "svelte",
+                "sql",
+                "toml",
+                "tsx",
+                "typescript",
+                "vimdoc",
+                "yaml",
+            },
+            ignore_install = { "php" },
+            sync_install = false,
+            auto_install = false,
+            autotag = {
+                enable = true,
+            },
+            highlight = {
+                enable = true,
+                extended_mode = true,
+                disable = function(_, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<c-space>",
+                    node_incremental = "<c-space>",
+                    scope_incremental = "<c-s>",
+                    node_decremental = "<c-backspace>",
+                },
+            },
+            playground = {
+                enable = true,
+            },
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { "BufWrite", "CursorHold" },
+            },
+        })
+    end,
 }
