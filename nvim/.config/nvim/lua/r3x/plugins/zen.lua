f9f2943bfbcbe38b9ec5a650cacb0645e0c40f25
edr3x <dhunganaannanta@gmail.com>
2023-09-09 16:19:15 +0545

    feat: zenmode plugin and terminal font increase

---
+++ nvim/.config/nvim/lua/r3x/plugins/zen.lua
@@ -0,0 +1,28 @@
+return {
+    "folke/zen-mode.nvim",
+    event = "BufReadPre",
+    opts = {
+        plugins = {
+            options = {
+                enabled = true,
+                ruler = true,
+                showcmd = false,
+            },
+            gitsigns = { enabled = false },
+            tmux = { enabled = true },
+        },
+    },
+    keys = {
+        {
+            "<leader>zz",
+            function()
+                require("zen-mode").toggle({
+                    window = {
+                        width = 0.55,
+                    },
+                })
+            end,
+            desc = "Zen mode",
+        },
+    },
+}
