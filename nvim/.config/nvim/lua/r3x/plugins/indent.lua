a22a8ff3ee0212180f194e5610f20817ccc27bd3
edr3x <dhunganaannanta@gmail.com>
2023-09-28 19:47:35 +0545

    chore: made config compatible with indent_blankline v3.0

--- nvim/.config/nvim/lua/r3x/plugins/indent.lua
+++ nvim/.config/nvim/lua/r3x/plugins/indent.lua
@@ -1,12 +1,15 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     ft = { "yaml" },
     opts = {
-        char = "┊",
-        use_treesitter = true,
-        space_char_blankline = " ",
-        show_current_context = false,
-        show_current_context_start = false,
-        buftype_exclude = { "nofile", "terminal" },
+        indent = { char = "┊" },
+        scope = {
+            show_start = false,
+            show_end = false,
+        },
+        exclude = {
+            buftypes = { "terminal", "nofile" },
+        },
     },
 }
