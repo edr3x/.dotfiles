4d2d946870d875b9ded67c1d2e5fd083ddbae49d
edr3x <dhunganaannanta@gmail.com>
2023-11-18 15:09:52 +0545

    feat(nvim): get lsp progress message on bottom statusline

--- nvim/.config/nvim/lua/r3x/plugins/lualine.lua
+++ nvim/.config/nvim/lua/r3x/plugins/lualine.lua
@@ -21,6 +21,21 @@ return {
             color = { fg = "#d3d3d3" },
         }
 
+        local lsp_progress = {
+            function()
+                local lsp = vim.lsp.util.get_progress_messages()[1]
+                if lsp then
+                    local name = lsp.name or ""
+                    local msg = lsp.message or ""
+                    local title = lsp.title or ""
+                    local percentage = lsp.percentage or 0
+                    return string.format("%%<%s: %s %s (%s%%%%)", name, title, msg, percentage)
+                end
+                return ""
+            end,
+            color = { fg = "#d3d3d3" },
+        }
+
         local buffer = {
             "buffers",
             mode = 0,
@@ -68,7 +83,7 @@ return {
                 lualine_a = { "mode" },
                 lualine_b = { "branch" },
                 lualine_c = { buffer },
-                lualine_x = { diff, diagnostic },
+                lualine_x = { lsp_progress, diff, diagnostic },
                 lualine_y = { lspStatus, "filetype" },
                 lualine_z = { "progress" },
             },
