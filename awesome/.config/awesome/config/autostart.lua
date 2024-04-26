4bd35645de44df813fc21fb9e31ebf0a836ee63d
edr3x <dhunganaannanta@gmail.com>
2024-04-26 12:38:55 +0545

    feat: prevent screen from going black due to inactivity

--- awesome/.config/awesome/config/autostart.lua
+++ awesome/.config/awesome/config/autostart.lua
@@ -3,3 +3,6 @@ local autostart = require("awful").spawn.with_shell
 autostart("picom")
 autostart('setxkbmap -option "caps:escape"')
 autostart("nm-applet")
+autostart("xset s off")
+autostart("xset -dpms")
+autostart("xset s noblank")
