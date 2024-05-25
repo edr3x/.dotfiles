e5e19dad371c94b30cee085f6cd18d4d1f8dd65e
edr3x <dhunganaannanta@gmail.com>
2024-05-25 13:18:17 +0545

    feat(wezterm): config to my liking

---
+++ wezterm/.config/wezterm/theme.lua
@@ -0,0 +1,33 @@
+local colors = {
+	foreground = "#c0caf5",
+	background = "#000000",
+	cursor_bg = "#c0caf5",
+	cursor_border = "#c0caf5",
+	cursor_fg = "#000000",
+	selection_bg = "#ffffff",
+	selection_fg = "#000000",
+
+	ansi = {
+		"#15161e", -- black
+		"#f7768e", -- red
+		"#9ece6a", -- green
+		"#e0af68", -- yellow
+		"#7aa2f7", -- blue
+		"#bb9af7", -- magenta
+		"#7dcfff", -- cyan
+		"#a9b1d6", -- white
+	},
+
+	brights = {
+		"#414868", -- bright black
+		"#f7768e", -- bright red
+		"#9ece6a", -- bright green
+		"#e0af68", -- bright yellow
+		"#7aa2f7", -- bright blue
+		"#bb9af7", -- bright magenta
+		"#7dcfff", -- bright cyan
+		"#c0caf5", -- bright white
+	},
+}
+
+return colors
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,20 +1,20 @@
+local theme = require("theme")
 local wezterm = require("wezterm")
+
 return {
 	enable_tab_bar = false,
+	colors = theme,
 
-	color_scheme = "tokyonight",
+	-- disable font ligatures
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 
-	font_size = 13.0,
-	font = wezterm.font("Iosevka Nerd Font"),
+	font_size = 11,
+	font = wezterm.font_with_fallback({
+		"Fira Code Nerd Font",
+		"Caskaydia Cove Nerd Font",
+		"Iosevka Nerd Font",
+	}),
 
-	text_background_opacity = 1.0,
-	window_background_image = "/home/r3x/.config/awesome/themes/night/wall/0220.jpg",
-	window_background_opacity = 1.0,
-	window_background_image_hsb = {
-		brightness = 0.6,
-		hue = 1.0,
-		saturation = 1.0,
-	},
-
-	default_prog = { "/usr/bin/zsh", "-l" },
+	text_background_opacity = 0.7,
+	window_background_opacity = 0.45,
 }
